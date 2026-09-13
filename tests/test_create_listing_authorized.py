11ede7ece34bd4cc754af2adf80b4b7a14181a3b
Korochkin Nikolay <korochkinnickolay@yandex.ru>
2026-09-13 14:29:46 +0300

    MR #1

--- tests/test_create_listing_authorized.py
+++ tests/test_create_listing_authorized.py
@@ -4,25 +4,26 @@ from urls import Urls
 from locators  import TestLocators
 import data
 
+class TestCreateListingAuthorized:
+    def test_create_listing_authorized(self,driver):
+        driver.get(Urls.HOME_URL)
+        driver.find_element(*TestLocators.LOGIN_BUTTON).click()
+        driver.find_element(*TestLocators.INPUT_EMAIL_BUTTON).send_keys(data.VALID_EMAIL)
+        driver.find_element(*TestLocators.INPUT_PASSWORD_BUTTON).send_keys(data.VALID_PASSWORD)
+        driver.find_element(*TestLocators.LOGIN_SUBMIT_BUTTON).click()
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(TestLocators.LOGIN))
+        cards_before = driver.find_elements(*TestLocators.LISTING_CARDS)
+        count_before = len(cards_before)
+        driver.find_element(*TestLocators.CREATE_LISTING).click()
+        driver.find_element(*TestLocators.NAME_INPUT).send_keys(data.VALID_NAME)
+        driver.find_element(*TestLocators.DESCRIPTION_INPUT).send_keys(data.VALID_DESCRIPTION)
+        driver.find_element(*TestLocators.PRICE_INPUT).send_keys(data.VALID_PRICE)
+        driver.find_element(*TestLocators.CATEGORY_SELECT_BUTTON).click()
+        driver.find_element(*TestLocators.CATEGORY_SUBMIT_BUTTON).click()
+        driver.find_element(*TestLocators.CITY_SELECT_BUTTON).click()
+        driver.find_element(*TestLocators.CITY_SUBMIT_BUTTON).click()
+        driver.find_element(*TestLocators.PUBLISH_BUTTON).click()
+        cards_after = driver.find_elements(*TestLocators.LISTING_CARDS)
+        count_after = len(cards_after)
+        assert count_after == count_before + 1, 'Количество созданных объявлений не изменилось'
 
-def test_create_listing_authorized(driver):
-    driver.get(Urls.HOME_URL)
-    driver.find_element(*TestLocators.LOGIN_BUTTON).click()
-    driver.find_element(*TestLocators.INPUT_EMAIL_BUTTON).send_keys(data.VALID_EMAIL)
-    driver.find_element(*TestLocators.INPUT_PASSWORD_BUTTON).send_keys(data.VALID_PASSWORD)
-    driver.find_element(*TestLocators.LOGIN_SUBMIT_BUTTON).click()
-    WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(TestLocators.LOGIN))
-    cards_before = driver.find_elements(*TestLocators.LISTING_CARDS)
-    count_before = len(cards_before)
-    driver.find_element(*TestLocators.CREATE_LISTING).click()
-    driver.find_element(*TestLocators.NAME_INPUT).send_keys(data.VALID_NAME)
-    driver.find_element(*TestLocators.DESCRIPTION_INPUT).send_keys(data.VALID_DESCRIPTION)
-    driver.find_element(*TestLocators.PRICE_INPUT).send_keys(data.VALID_PRICE)
-    driver.find_element(*TestLocators.CATEGORY_SELECT_BUTTON).click()
-    driver.find_element(*TestLocators.CATEGORY_SUBMIT_BUTTON).click()
-    driver.find_element(*TestLocators.CITY_SELECT_BUTTON).click()
-    driver.find_element(*TestLocators.CITY_SUBMIT_BUTTON).click()
-    driver.find_element(*TestLocators.PUBLISH_BUTTON).click()
-    cards_after = driver.find_elements(*TestLocators.LISTING_CARDS)
-    count_after = len(cards_after)
-    assert count_after == count_before + 1, 'Количество созданных объявлений не изменилось'
--- tests/test_register_existing_user.py
+++ tests/test_register_existing_user.py
@@ -2,19 +2,20 @@ from urls import Urls
 from locators  import TestLocators
 import data
 
-def test_register_existing_user(driver):
-    driver.get(Urls.HOME_URL)
-    driver.find_element(*TestLocators.LOGIN_BUTTON).click()
-    driver.find_element(*TestLocators.NO_ACCOUNT_BUTTON).click()
-    driver.find_element(*TestLocators.INPUT_EMAIL_BUTTON).send_keys(data.VALID_EMAIL)
-    driver.find_element(*TestLocators.INPUT_PASSWORD_BUTTON).send_keys(data.VALID_PASSWORD)
-    driver.find_element(*TestLocators.REPEAT_PASSWORD_BUTTON).send_keys(data.VALID_PASSWORD)
-    driver.find_element(*TestLocators.CREATE_ACCOUNT).click()
-    error_text = driver.find_element(*TestLocators.INVALID_EMAIL_ERROR)
-    email_error = driver.find_element(*TestLocators.EMAIL_ERROR)
-    password_error = driver.find_element(*TestLocators.PASSWORD_ERROR)
-    repeat_error = driver.find_element(*TestLocators.REPEAT_PASSWORD_ERROR)
-    assert error_text.is_displayed(), "текст ошибки не выводится"
-    assert email_error.is_displayed(), "нет красной обводки у поля Email"
-    assert password_error.is_displayed(), "нет красной обводки у поля Пароль"
-    assert repeat_error.is_displayed(), "нет красной обводки у поля Повторите пароль"
+class 	TestRegisterExistingUser:
+    def test_register_existing_user(self,driver):
+        driver.get(Urls.HOME_URL)
+        driver.find_element(*TestLocators.LOGIN_BUTTON).click()
+        driver.find_element(*TestLocators.NO_ACCOUNT_BUTTON).click()
+        driver.find_element(*TestLocators.INPUT_EMAIL_BUTTON).send_keys(data.VALID_EMAIL)
+        driver.find_element(*TestLocators.INPUT_PASSWORD_BUTTON).send_keys(data.VALID_PASSWORD)
+        driver.find_element(*TestLocators.REPEAT_PASSWORD_BUTTON).send_keys(data.VALID_PASSWORD)
+        driver.find_element(*TestLocators.CREATE_ACCOUNT).click()
+        error_text = driver.find_element(*TestLocators.INVALID_EMAIL_ERROR)
+        email_error = driver.find_element(*TestLocators.EMAIL_ERROR)
+        password_error = driver.find_element(*TestLocators.PASSWORD_ERROR)
+        repeat_error = driver.find_element(*TestLocators.REPEAT_PASSWORD_ERROR)
+        assert error_text.is_displayed(), "текст ошибки не выводится"
+        assert email_error.is_displayed(), "нет красной обводки у поля Email"
+        assert password_error.is_displayed(), "нет красной обводки у поля Пароль"
+        assert repeat_error.is_displayed(), "нет красной обводки у поля Повторите пароль"
